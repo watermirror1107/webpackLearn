993f410788d6afbada419017fb71ba7504aac208
json <ooo88888@live.cn>
2018-11-21 22:46:11 +0800

    postcss的配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')
+        require('autoprefixer')({
+            browsers : ['last 30 versions', "> 2%", "Firefox >= 10", "ie 6-11"]
+        })
     ]
 }
