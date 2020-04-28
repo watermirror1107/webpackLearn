31c0395156f822c88ae66bbf32daeceec147bfa4
Jason <ooo88888@live.cn>
2020-04-28 10:33:26 +0800

    删除错误注释

--- server/dev-server.js
+++ server/dev-server.js
@@ -9,7 +9,7 @@ let obj1={
         contentBase: './public/',//本地服务其所加载的页面所在的目录
         historyApiFallback: true,//不跳转
         compress: true,//开启gzip，这个是测试服务器用的，不需要配合CompressionWebpackPlugin插件，插件是拿来打包用的
-        // inline: false,//自动刷新，默认开启，不用的时候需要自动关闭,和热更冲突，用热更的是需要关闭；
+        // inline: false,
         hot: true,//开启热更
         noInfo: false
     },
